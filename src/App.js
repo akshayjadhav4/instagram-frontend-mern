1a1f9823e910fc581e72c34bc0493ced6054fae6
AKSHAY <akshayjadhav461999@gmail.com>
2020-10-19 14:23:41 +0530

    route added for Home

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
 import "./App.css";
-
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
