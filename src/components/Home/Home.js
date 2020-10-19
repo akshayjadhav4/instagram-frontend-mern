3e28c72dae1afc5e15e61cb926ad984472e955b4
AKSHAY <akshayjadhav461999@gmail.com>
2020-10-19 14:23:27 +0530

    basic home component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+import Base from "../Base/Base";
+function Home() {
+  return (
+    <Base>
+      <div className="home">
+        <h1>Home</h1>
+      </div>
+    </Base>
+  );
+}
+
+export default Home;
