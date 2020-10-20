6c2f1f0e8613ab9d950099f4f2ae6099d2da6769
AKSHAY <akshayjadhav461999@gmail.com>
2020-10-20 14:03:54 +0530

    profile middle menu section added

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -2,6 +2,11 @@ import React from "react";
 import "./Profile.css";
 import Base from "../Base/Base";
 import ProfileHeader from "../ProfileHeader/ProfileHeader";
+import GridOnSharpIcon from "@material-ui/icons/GridOnSharp";
+import LiveTvSharpIcon from "@material-ui/icons/LiveTvSharp";
+import TurnedInNotIcon from "@material-ui/icons/TurnedInNot";
+import AccountBoxOutlinedIcon from "@material-ui/icons/AccountBoxOutlined";
+import { Button } from "@material-ui/core";
 function Profile() {
   return (
     <Base>
@@ -18,6 +23,26 @@ function Profile() {
             erat sed diam cursus imperdiet. Nam dictum fringilla lacus quis
             malesuada."
           />
+          <hr className="profile__divider" />
+          <div className="profile__menu">
+            <Button
+              startIcon={<GridOnSharpIcon />}
+              variant="text"
+              className="profile__menu--active"
+            >
+              POSTS
+            </Button>
+            <Button startIcon={<LiveTvSharpIcon />} variant="text">
+              IGTV
+            </Button>
+            <Button startIcon={<TurnedInNotIcon />} variant="text">
+              SAVED
+            </Button>
+            <Button startIcon={<AccountBoxOutlinedIcon />} variant="text">
+              TAGGED
+            </Button>
+          </div>
+          <hr className="profile__divider" />
         </div>
       </div>
     </Base>
