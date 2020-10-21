1818fcb56686e32b82bea4f720b16dc8ab0bf789
AKSHAY <akshayjadhav461999@gmail.com>
2020-10-21 16:36:26 +0530

    api calls for getPosts ,like, unlike

--- src/api/posts/postsApiCalls.js
+++ src/api/posts/postsApiCalls.js
@@ -30,3 +30,50 @@ export const createPost = (userId, token, post) => {
     })
     .catch((error) => console.log(error));
 };
+
+// to show on user feed
+export const getAllPostsOfFollowing = (userId, token) => {
+  return fetch(`${API}/user/getAllPostsOfFollowing/${userId}`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((error) => console.log(error));
+};
+
+export const likePost = (postId, userId, token) => {
+  return fetch(`${API}/post/like/${postId}/${userId}`, {
+    method: "PUT",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ postId: postId }),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((error) => console.log(error));
+};
+
+export const unlikePost = (postId, userId, token) => {
+  return fetch(`${API}/post/unlike/${postId}/${userId}`, {
+    method: "PUT",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ postId: postId }),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .catch((error) => console.log(error));
+};
